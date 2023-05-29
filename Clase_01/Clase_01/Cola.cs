7079247b7295549a9ad96e9889db320a858cded7
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-05-29 12:51:15 -0300

    Actualización Clase 1

---
+++ Clase_01/Clase_01/Cola.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_01
+{
+    /// <summary>
+    /// Representa una colección de objetos de tipo primero en entrar, primero en salir.
+    /// </summary>
+    public class Cola<T>
+    {
+        private List<T> datos = new List<T>();
+
+        public void encolar(T dato)
+        {
+            datos.Add(dato);
+        }
+
+        public T desencolar()
+        {
+            T desencolado = datos[0];
+
+            datos.Remove(desencolado);
+
+            return desencolado;
+            
+        }
+    }
+
+}
