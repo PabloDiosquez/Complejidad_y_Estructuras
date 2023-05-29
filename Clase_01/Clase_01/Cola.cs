13c747b75021a65525d0de734ca656129be03a80
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-05-29 14:35:02 -0300

    Actualización Clase 1

--- Clase_01/Clase_01/Cola.cs
+++ Clase_01/Clase_01/Cola.cs
@@ -13,11 +13,19 @@ namespace Clase_01
     {
         private List<T> datos = new List<T>();
 
+        /// <summary>
+        /// Agrega el dato en la cola de datos.
+        /// </summary>
+        /// <param name="dato">El dato a encolar - Tipo: T</param>
         public void encolar(T dato)
         {
             datos.Add(dato);
         }
 
+        /// <summary>
+        /// Desencola el primer dato en la cola de datos.
+        /// </summary>
+        /// <returns>Un dato de tipo T</returns>
         public T desencolar()
         {
             T desencolado = datos[0];
@@ -26,6 +34,24 @@ namespace Clase_01
 
             return desencolado;
             
+        }
+
+        /// <summary>
+        /// Describe el primer dato en la cola de datos.
+        /// </summary>
+        /// <returns>Un dato de tipo T</returns>
+        public T verTope() 
+        {
+            return datos[0];
+        }
+
+        /// <summary>
+        /// Indica si la cola de datos está vacía.
+        /// </summary>
+        /// <returns>Un valor de tipo bool</returns>
+        public bool esVacia()
+        {
+            return datos.Count == 0;
         }
     }
 
