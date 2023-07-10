b2fc7adfb17391fa92fe2d027b1ff880c2b3668d
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-10 11:53:41 -0300

    Actualización Clase 2 - Árbol Binario

---
+++ Clase_02/Clase_02/ArbolBinario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    public class ArbolBinario<T>
+    {
+        // Atributos
+
+        private NodoBinario<T> raiz;
+
+        // Constructores 
+
+        public ArbolBinario(T dato) 
+        {
+            raiz = new NodoBinario<T>(dato);
+        }
+
+        private ArbolBinario(NodoBinario<T> raiz) // ➡ Método auxiliar para utilizar en los gethijos...
+        {
+            this.raiz = raiz;   
+        }
+
+        // Getters y Setters
+
+        private NodoBinario<T> getRaiz()
+        {
+            return raiz;
+        }
+
+        public T getDatoRaiz()
+        {
+            return raiz.getDato();
+        }
+
+        public ArbolBinario<T> getHijoIzquierdo()
+        {
+            return new ArbolBinario<T>(raiz.getHijoIzquierdo());
+        }
+
+        public ArbolBinario<T> getHijoDerecho()
+        {
+            return new ArbolBinario<T>(raiz.getHijoDerecho());
+        }
+
+        public void setHijoIzquierdo(ArbolBinario<T> hijoIzquierdo)
+        {
+            raiz.setHijoIzquierdo(hijoIzquierdo.getRaiz());
+        }
+
+        public void setHijoDerecho(ArbolBinario<T> hijoDerecho)
+        {
+            raiz.setHijoDerecho(hijoDerecho.getRaiz());
+        }
+
+    }
+}
