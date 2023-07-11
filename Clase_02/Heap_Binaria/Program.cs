927c7d83d3bc6352e6786dcfca065b8dc4063b44
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-11 10:29:49 -0300

    Actualización Data Structures in JS

--- Clase_02/Heap_Binaria/Program.cs
+++ Clase_02/Heap_Binaria/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Heap_Binaria
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
---
+++ Data Structures and Algorithms in JavaScript/01_Stacks.js
@@ -0,0 +1,26 @@
+// Stacks 📚
+
+// Functions: push, pop, peek, length or size
+
+var letters = []; // this is our stack 
+
+var word = "racecar";
+
+var rword = "";
+
+// Put letters of word into stack
+for(var i = 0; i < word.length; i++){
+    letters.push(word[i]);
+}
+
+// pop off the stack in reverse order 
+for (let i = 0; i < letters.length; i++) {
+    rword += letters.pop()
+}
+
+if (word === rword){
+    console.log(word + " is a paplidrome");
+}
+else{
+    console.log(word + " is not a palindrome");
+}
